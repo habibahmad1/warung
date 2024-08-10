1c4d8c6b2394a7d95292cde266cafd466c0e9f69
habibahmad1 <oregairu2597@gmail.com>
2024-08-10 16:07:46 +0700

    first

---
+++ app/Http/Controllers/SembakoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\sembako;
+use App\Http\Requests\StoresembakoRequest;
+use App\Http\Requests\UpdatesembakoRequest;
+
+class SembakoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoresembakoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(sembako $sembako)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(sembako $sembako)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatesembakoRequest $request, sembako $sembako)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(sembako $sembako)
+    {
+        //
+    }
+}

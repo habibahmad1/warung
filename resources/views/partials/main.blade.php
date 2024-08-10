8c1656a93fad4d5b6502a5072e6fb58c2b837657
habibahmad1 <oregairu2597@gmail.com>
2024-08-11 00:29:48 +0700

    add navbar

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -4,11 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Warung</title>
-    <link rel="stylesheet" href="">
-    <link rel="icon" href="">
+    <title>Warung Gokil</title>
+    {{-- Fav Ico --}}
+    <link rel="icon" href="https://cdn.pixabay.com/photo/2021/05/25/02/03/restaurant-6281067_1280.png">
+    {{-- CSS Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{-- My CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    <h1>Welcome</h1>
+
+    <nav>
+        <h3><a href="">Warung <span class="text-warning">Gokil</span></a> </h3>
+        <ul>
+            <a href="#home"><li>Home</li></a>
+            <a href="#about"><li>Best Seller</li></a>
+            <a href="#contact"><li>Menu</li></a>
+            <a href="#contact"><li>Reviews</li></a>
+        </ul>
+        <a href="" class="login">Login</a>
+    </nav>
+
+    <div class="container-main">
+        @yield('content')
+    </div>
+
+    {{-- My JS --}}
+    <script src="{{ asset('js/script.js') }}"></script>
+
+    {{-- Bootstrap JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>

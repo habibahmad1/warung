52259811c31a01a029df6a76ff424e0c58f603fc
habibahmad1 <oregairu2597@gmail.com>
2024-08-13 22:35:19 +0700

    home page design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,17 @@
 @section("content")
 
     <div class="container-home">
-        <h1 style="mt-5">Halaman Home</h1>
+
+        <div class="card-home">
+            <div class="desc-home">
+                <h3 class="text-center">Welcome Customer</h3>
+                <hr>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit reprehenderit earum aspernatur at facere quas dolorem error doloremque eum nostrum? Ducimus ipsam animi voluptate, illo quod necessitatibus modi iste odio veniam deserunt enim, et ex voluptatem iusto atque natus quis veritatis fugiat libero officiis laboriosam laborum! Reiciendis recusandae iusto quidem!</p>
+            </div>
+            <div class="hero">
+                <img src="{{ asset('img/hero.png') }}" alt="">
+            </div>
+        </div>
     </div>
 
 @endsection

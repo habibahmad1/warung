48052e3a064627a874ce407b5b514a8b6b461499
habibahmad1 <oregairu2597@gmail.com>
2024-08-16 18:58:14 +0700

    add review page design

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -1,8 +1,92 @@
 @extends("partials.main")
 @section("content")
 
-    <div class="container-home">
-        <h1 style="mt-5">Halaman Reviews</h1>
+<h3 class="text-center my-5">Review tentang Warung Gokil</h3>
+    <div class="container-reviews">
+        <div class="card-komentar">
+              <h5>Name People</h5>
+              <p>Bagus sekarang jadi mudah pesan makan online</p>
+              <div class="rate-star">
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                {{-- @for ($i = 0; $i < $rating; $i++)
+                    <i class="fa-solid fa-star text-warning"></i>
+                @endfor --}}
+              </div>
+        </div>
+        <div class="card-komentar">
+              <h5>Name People</h5>
+              <p>Bagus sekarang jadi mudah pesan makan online</p>
+              <div class="rate-star">
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                {{-- @for ($i = 0; $i < $rating; $i++)
+                    <i class="fa-solid fa-star text-warning"></i>
+                @endfor --}}
+              </div>
+        </div>
+        <div class="card-komentar">
+              <h5>Name People</h5>
+              <p>Bagus sekarang jadi mudah pesan makan online</p>
+              <div class="rate-star">
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                {{-- @for ($i = 0; $i < $rating; $i++)
+                    <i class="fa-solid fa-star text-warning"></i>
+                @endfor --}}
+              </div>
+        </div>
+        <div class="card-komentar">
+              <h5>Name People</h5>
+              <p>Bagus sekarang jadi mudah pesan makan online</p>
+              <div class="rate-star">
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                {{-- @for ($i = 0; $i < $rating; $i++)
+                    <i class="fa-solid fa-star text-warning"></i>
+                @endfor --}}
+              </div>
+        </div>
+        <div class="card-komentar">
+              <h5>Name People</h5>
+              <p>Bagus sekarang jadi mudah pesan makan online</p>
+              <div class="rate-star">
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                {{-- @for ($i = 0; $i < $rating; $i++)
+                    <i class="fa-solid fa-star text-warning"></i>
+                @endfor --}}
+              </div>
+        </div>
+        <div class="card-komentar">
+              <h5>Name People</h5>
+              <p>Bagus sekarang jadi mudah pesan makan online</p>
+              <div class="rate-star">
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                <i class="fa-solid fa-star text-warning"></i>
+                {{-- @for ($i = 0; $i < $rating; $i++)
+                    <i class="fa-solid fa-star text-warning"></i>
+                @endfor --}}
+              </div>
+        </div>
     </div>
 
 @endsection

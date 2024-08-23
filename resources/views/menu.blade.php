26b9c0075a7fad6fa749b2708ae7d72090e179a0
habibahmad1 <oregairu2597@gmail.com>
2024-08-23 21:21:57 +0700

    add design menu page

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,8 +1,74 @@
 @extends("partials.main")
 @section("content")
 
-    <div class="container-home">
-        <h1 style="mt-5">Halaman Menu</h1>
+<h1 class="text-center mb-5">Menu Warung Gokil</h1>
+    <div class="container-menu">
+        <div class="card-menu">
+            <h3>Name Menu</h3>
+            <div class="img-menu">
+                <img src="https://cdn.pixabay.com/photo/2024/05/06/17/02/burgers-8743798_640.jpg" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam sed architecto asperiores corporis ab odit natus libero fugit dicta!</p>
+            <h5>Rp.20,000</h5>
+            <div class="pesan-menu">
+                <a href="">Pesan</a>
+            </div>
+        </div>
+        <div class="card-menu">
+            <h3>Name Menu</h3>
+            <div class="img-menu">
+                <img src="https://cdn.pixabay.com/photo/2024/05/06/17/02/burgers-8743798_640.jpg" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam sed architecto asperiores corporis ab odit natus libero fugit dicta!</p>
+            <h5>Rp.20,000</h5>
+            <div class="pesan-menu">
+                <a href="">Pesan</a>
+            </div>
+        </div>
+        <div class="card-menu">
+            <h3>Name Menu</h3>
+            <div class="img-menu">
+                <img src="https://cdn.pixabay.com/photo/2024/05/06/17/02/burgers-8743798_640.jpg" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam sed architecto asperiores corporis ab odit natus libero fugit dicta!</p>
+            <h5>Rp.20,000</h5>
+            <div class="pesan-menu">
+                <a href="">Pesan</a>
+            </div>
+        </div>
+        <div class="card-menu">
+            <h3>Name Menu</h3>
+            <div class="img-menu">
+                <img src="https://cdn.pixabay.com/photo/2024/05/06/17/02/burgers-8743798_640.jpg" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam sed architecto asperiores corporis ab odit natus libero fugit dicta!</p>
+            <h5>Rp.20,000</h5>
+            <div class="pesan-menu">
+                <a href="">Pesan</a>
+            </div>
+        </div>
+        <div class="card-menu">
+            <h3>Name Menu</h3>
+            <div class="img-menu">
+                <img src="https://cdn.pixabay.com/photo/2024/05/06/17/02/burgers-8743798_640.jpg" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam sed architecto asperiores corporis ab odit natus libero fugit dicta!</p>
+            <h5>Rp.20,000</h5>
+            <div class="pesan-menu">
+                <a href="">Pesan</a>
+            </div>
+        </div>
+        <div class="card-menu">
+            <h3>Name Menu</h3>
+            <div class="img-menu">
+                <img src="https://cdn.pixabay.com/photo/2024/05/06/17/02/burgers-8743798_640.jpg" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos, quisquam sed architecto asperiores corporis ab odit natus libero fugit dicta!</p>
+            <h5>Rp.20,000</h5>
+            <div class="pesan-menu">
+                <a href="">Pesan</a>
+            </div>
+        </div>
     </div>
 
 @endsection

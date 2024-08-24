34a1a00b8f0ffb111dd62c405243ad923acc2dad
habibahmad1 <oregairu2597@gmail.com>
2024-08-24 18:23:32 +0700

    add design best page

--- resources/views/best.blade.php
+++ resources/views/best.blade.php
@@ -1,8 +1,40 @@
 @extends("partials.main")
 @section("content")
 
-    <div class="container-home">
-        <h1 style="mt-5">Halaman Best Seller</h1>
+    <div class="container-best">
+        <div class="card-best">
+            <div class="desc-best">
+                <h3>Name Food</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae blanditiis, repellendus assumenda delectus consectetur deleniti aliquid fugit possimus alias placeat?</p>
+                <p>Sudah dibeli <span class="text-black"><b>20578</b></span> kali</p>
+                <h6 class="best-pesan">Pesan Sekarang</h6>
+            </div>
+            <div class="img-best">
+                <img src="https://cdn.pixabay.com/photo/2024/05/06/17/02/burgers-8743798_640.jpg" alt="">
+            </div>
+        </div>
+        <div class="card-best">
+            <div class="desc-best">
+                <h3>Name Food</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae blanditiis, repellendus assumenda delectus consectetur deleniti aliquid fugit possimus alias placeat?</p>
+                <p>Sudah dibeli <span class="text-black"><b>20578</b></span> kali</p>
+                <h6 class="best-pesan">Pesan Sekarang</h6>
+            </div>
+            <div class="img-best">
+                <img src="https://cdn.pixabay.com/photo/2024/05/06/17/02/burgers-8743798_640.jpg" alt="">
+            </div>
+        </div>
+        <div class="card-best">
+            <div class="desc-best">
+                <h3>Name Food</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae blanditiis, repellendus assumenda delectus consectetur deleniti aliquid fugit possimus alias placeat?</p>
+                <p>Sudah dibeli <span class="text-black"><b>20578</b></span> kali</p>
+                <h6 class="best-pesan">Pesan Sekarang</h6>
+            </div>
+            <div class="img-best">
+                <img src="https://cdn.pixabay.com/photo/2024/05/06/17/02/burgers-8743798_640.jpg" alt="">
+            </div>
+        </div>
     </div>
 
 @endsection

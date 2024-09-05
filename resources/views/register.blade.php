7a83d97e9a2c9bc7610bab08a2e8bf0b70c04119
habibahmad1 <oregairu2597@gmail.com>
2024-09-05 22:12:39 +0700

    add required register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,9 +6,9 @@
         <form action="{{ route('register.store') }}" method="POST">
             @csrf
             <input type="text" placeholder="Fullname" name="fullname" id="fullname" required>
-            <input type="text" placeholder="No WhatsApp" name="nowa" id="nowa">
-            <input type="email" placeholder="Email" name="email" id="email">
-            <input type="password" placeholder="Password" name="password" id="password">
+            <input type="text" placeholder="No WhatsApp" name="nowa" id="nowa" required>
+            <input type="email" placeholder="Email" name="email" id="email" required>
+            <input type="password" placeholder="Password" name="password" id="password" required>
             <input type="password" placeholder="Ketik Ulang Password" name="password_confirmation" id="password_confirmation" required>
             <button type="submit">Buat Akun</button>
             <p class="text-center">Sudah punya akun?<br> <a href="/login">Login Sekarang</a></p>
